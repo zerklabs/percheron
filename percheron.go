dd836876a2c40ef1c8e20da4b1041d028b95bf60
Robin Harper <cabrel@zerklabs.com>
2014-02-20 02:50:05 -0500

    functions for constructing the basic percheron types

--- percheron.go
+++ percheron.go
@@ -5,7 +5,7 @@ import (
 	"time"
 )
 
-type UserInfo struct {
+type User struct {
 	Email   string
 	Created time.Time
 	ID      *uuid.UUID
@@ -18,8 +18,19 @@ type Bucket struct {
 	ID      *uuid.UUID
 }
 
+type ObjMetadata struct {
+	Name         string
+	Size         int64
+	Created      time.Time
+	Modified     time.Time
+	Owner        uuid.UUID
+	ID           uuid.UUID
+	Checksum     string
+	ChecksumHash string
+}
+
 // TODO(rch): add directory creation
-func (user *UserInfo) NewBucket(name string) (*Bucket, error) {
+func (user *User) NewBucket(name string) (*Bucket, error) {
 	id, err := uuid.NewV4()
 
 	if err != nil {
@@ -34,4 +45,40 @@ func (user *UserInfo) NewBucket(name string) (*Bucket, error) {
 	bucket.ID = id
 
 	return bucket, nil
+}
+
+// TODO(rch): add directory creation
+func NewUserInfo(email string) (*User, error) {
+	id, err := uuid.NewV4()
+
+	if err != nil {
+		return &User{}, err
+	}
+
+	user := new(User)
+
+	user.Email = email
+	user.Created = time.Now()
+	user.ID = id
+
+	return user, nil
+}
+
+// TODO(rch): add directory creation
+func (bucket *Bucket) NewObject(name string) (*ObjMetadata, error) {
+	id, err := uuid.NewV4()
+
+	if err != nil {
+		return &ObjMetadata{}, err
+	}
+
+	obj := new(ObjMetadata)
+
+	obj.Name = name
+	obj.Created = time.Now()
+	obj.Created = obj.Created
+	obj.Owner = bucket.Owner
+	obj.ID = id
+
+	return obj, nil
 }
